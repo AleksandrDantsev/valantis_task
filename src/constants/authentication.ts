e91fbe50e655e61633575d864521b93b019e2db5
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-03-02 16:24:13 +0300

    feat: cheat file for auth-data

---
+++ src/constants/authentication.ts
@@ -0,0 +1,8 @@
+import { getDate } from "../helpers/formatDate"
+
+const PASSWORD: string = "Valantis"
+const TIMESTAMP: string = getDate() // format date: '20240302'
+
+
+
+export { PASSWORD, TIMESTAMP }

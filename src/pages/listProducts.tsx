eb4976e7263bfdfc2a11c77db1073d511265ab54
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-03-02 16:23:18 +0300

    feat(ListProduct): create page

---
+++ src/pages/listProducts.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { md5 } from 'js-md5';
+import { PASSWORD, TIMESTAMP } from '../constants/authentication';
+
+
+export const ListProducts: React.FC = () => {
+ 
+
+    return (
+        <>
+        </>
+    )
+}
+
+
+

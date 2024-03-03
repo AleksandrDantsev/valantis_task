f8fdd6a78c9d1b783edc50cb7e2346e0fb467ee6
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-03-03 11:08:28 +0300

    refactor(listProduct): substitute key access

--- src/pages/listProducts.tsx
+++ src/pages/listProducts.tsx
@@ -1,14 +1,23 @@
 import React from "react";
-import { md5 } from 'js-md5';
-import { PASSWORD, TIMESTAMP } from '../constants/authentication';
+import { KEY_ACCES } from '../constants/authentication';
+import { fetchCustom } from "../helpers/fetchData";
 
+const fetchValantisURL = fetchCustom("https://api.valantis.store:41000/", KEY_ACCES)
 
 export const ListProducts: React.FC = () => {
+    
+
+     fetchValantisURL("get_ids", {"offset": 0, "limit": 1000})
+        
+
+    
+  
  
 
     return (
-        <>
-        </>
+        <div>
+
+        </div>
     )
 }
 

2b7c71cd4eb5589db67c0b4ba40f84cb4f4df08a
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-03-09 21:43:35 +0300

    feat: add import image

---
+++ src/imports/importsImage.ts
@@ -0,0 +1,28 @@
+import colie from "../assets/colie.jpg"
+import ring from "../assets/ring.webp"
+import braslet from "../assets/braslet.jpg"
+import brosh from "../assets/brosh.png"
+import sergi from "../assets/sergi.jpg"
+import culon from "../assets/sergi.jpg"
+import chain from "../assets/chain.jpg"
+import complect from "../assets/complect.jpg"
+import stable from "../assets/stable.jpg"
+import bej from "../assets/bej.jpg"
+
+const importsImage: {[key: string]: string} = {
+    "колье": colie,
+    "кольцо": ring,
+    'браслет': braslet,
+    "брошь": brosh,
+    "серьги": sergi,
+    "кулон": culon,
+    "цепь": chain,
+    "комплект": complect,
+    "зажим": stable,
+    "бижутерия": bej,
+}
+
+
+
+
+export { importsImage }

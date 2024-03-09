03a43529c72d8fe59a3183985cb9146df301cbb9
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-03-09 12:09:59 +0300

    add type for fetch-products

---
+++ src/types/fetchTypes.ts
@@ -0,0 +1,11 @@
+type productsInfoType = {
+    result: [
+        brand: null | string,
+        id: string,
+        price: string,
+        product: string,
+    ] | []
+}
+
+
+export type { productsInfoType }

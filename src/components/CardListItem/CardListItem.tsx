8bbd33381e1ef7e9b1ac52ca76ed578bcaf97591
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-03-09 21:46:03 +0300

    chore: update class name

--- src/components/CardListItem/CardListItem.tsx
+++ src/components/CardListItem/CardListItem.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import st from "./CardListItem.module.scss"
+import { checkTypeProduct } from "../../helpers/importImages";
 
 interface ICardListItem {
     brand: null | string;
@@ -12,13 +13,13 @@ interface ICardListItem {
 
 export const CardListItem: React.FC<ICardListItem> = ({brand, id, price, product}) => {
 
-
     return (
         <div className={st.card_list_item}>
-            {Boolean(brand) && brand}
-            <div>{product}</div>
-            <div>{price}</div>    
-            <div>{id}</div>
+            {Boolean(brand) && <div className={st.brand}>{brand}</div>}
+            <img className={st.backgr_image} src={checkTypeProduct(product)} alt={product} />
+            <div className={st.title}><h3>{product}</h3></div>
+            <div className={st.price}>{price}р</div>    
+            <div className={st.id}>{id}</div>
         </div>
     )
 }

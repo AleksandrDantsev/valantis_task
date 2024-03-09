22bece0eb0d739990143ae9597d2367baf5c9901
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-03-09 21:43:05 +0300

    add tag a

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -13,19 +13,19 @@ const Pagination: React.FC<IPagination> = memo(({setPaginationNumber,
     const setPaginationNumberHandler = (e: React.MouseEvent<HTMLDListElement>): void => {
         e.preventDefault()
         const target = e.target as HTMLLIElement
-        if (target.tagName == "LI") setPaginationNumber(Number(target.textContent))
+        if (target.tagName == "A") setPaginationNumber(Number(target.textContent))
     } 
 
     const paintPaginationElement = () => {
-        const quantity = Math.ceil(lengthArrayProducts / quantityProductOnPage)
+        const quantity = Math.ceil(lengthArrayProducts / quantityProductOnPage) + 1;
 
         return new Array(quantity).fill(0).map((el, i) => el = i + 1)
     }
-    console.log(paintPaginationElement())
+
     return (
         <div>
             <ul onClick={setPaginationNumberHandler}>
-                {paintPaginationElement().map(el => <li><a href="#">{el}</a></li>)}
+                {paintPaginationElement().map(el => <li key={el}><a href="#">{el}</a></li>)}
             </ul>
         </div>
     )

e7066de3ecf5c3e4b42f439752eb3138a6f65d5d
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-03-09 12:08:15 +0300

    chore: eliminate missprint

--- src/constants/authentication.ts
+++ src/constants/authentication.ts
@@ -4,7 +4,7 @@ import { md5 } from 'js-md5';
 const PASSWORD: string = "Valantis"
 const TIMESTAMP: string = getDate() // format date: '20240302'
 
-const KEY_ACCES = md5(`${PASSWORD}_${TIMESTAMP}`)
+const KEY_ACCESS = md5(`${PASSWORD}_${TIMESTAMP}`)
 
 
-export { KEY_ACCES }
+export { KEY_ACCESS }

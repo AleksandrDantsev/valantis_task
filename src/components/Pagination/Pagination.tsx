f3df964d28be09ef1d970405b31e6cebaefde856
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-03-10 21:24:10 +0300

    feat: add function pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,31 +1,64 @@
-import React, { memo } from "react";
+import React, { memo, useState  } from "react";
 import st from "./Pagination.module.scss"
 
 interface IPagination {
     setPaginationNumber: (arg: number) => void,
     lengthArrayProducts: number;
     quantityProductOnPage: number;
+    paginationNumber: number;
 }
 
 const Pagination: React.FC<IPagination> = memo(({setPaginationNumber, 
                                                 lengthArrayProducts, 
-                                                quantityProductOnPage}) => {
+                                                quantityProductOnPage,
+                                                paginationNumber}) => {
+    const [numberPagBt, setNumberPagBt] = useState<string>('0')
 
     const setPaginationNumberHandler = (e: React.MouseEvent<HTMLDListElement>): void => {
         e.preventDefault()
         const target = e.target as HTMLLIElement
-        if (target.tagName == "A") setPaginationNumber(Number(target.textContent))
+        if (target.tagName == "A") {
+            setPaginationNumber(Number(target.textContent) - 1);
+
+            const dataNumber = target.getAttribute('data-number');
+            if (dataNumber) setNumberPagBt(dataNumber)
+        }
     } 
 
+    let quantity = Math.ceil(lengthArrayProducts / quantityProductOnPage);
+
     const paintPaginationElement = () => {
-        const quantity = Math.ceil(lengthArrayProducts / quantityProductOnPage) + 1;
-        return new Array(quantity).fill(0).map((el, i) => el = i + 1)
+        if (quantity > 10) {
+            return new Array(10).fill(0).map((_el, i) => {
+                if (i === 9) return quantity
+                if (paginationNumber < Math.round(9 / 2)) {
+                    return _el = i + 1
+                } 
+                else if (paginationNumber + Math.round(9 / 2) >= quantity || paginationNumber <= Math.round(9 / 2)) {
+                    return _el = quantity - (9 - i)
+                }
+                else {
+                    if (+numberPagBt < Math.floor(9 / 2)) {
+                        return _el = paginationNumber + (i - Math.floor(9 / 2 - 1))
+                    }
+                    else {
+                        return _el = paginationNumber + i - Math.floor(9 / 2 - 1)
+                    }
+                }
+            })
+        }
+        else {
+            return new Array(quantity).fill(0).map((_el, i) =>  _el = i + 1)
+        }
     }
 
     return (
         <div className={st.pagination}>
             <ul className={st.pagination_list} onClick={setPaginationNumberHandler}>
-                {paintPaginationElement().map(el => <li className={st.pagin_crumb} key={el}><a href="#">{el}</a></li>)}
+                {paintPaginationElement().map((el, i) => (
+                    <li className={st.pagin_crumb} key={el + i + Math.random()}>
+                        {paginationNumber <= quantity - 7 && i == 8 ? <p>---</p> : <a data-number={i+1} className={paginationNumber+1 == Number(el) ? st.active_bt : ''} href="#">{el}</a>}
+                    </li>))}
             </ul>
         </div>
     )

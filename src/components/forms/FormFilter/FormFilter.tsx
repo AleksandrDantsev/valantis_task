ec4f6651332abfaca685bfc1183a7bdebd37f5c1
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-03-10 12:39:55 +0300

    refactor: merge three filters into one

--- src/components/forms/FormFilter/FormFilter.tsx
+++ src/components/forms/FormFilter/FormFilter.tsx
@@ -1,28 +1,42 @@
 import React, { memo } from 'react';
 import st from "./FormFilter.module.scss";
-import SearchName from './SearchName/SearchName';
-import SearchBrand from './SearchBrand/SearchBrand';
-import SearchCost from './SearchCost/SearchCost';
+import { debounce } from '../../../helpers/debounce';
 
 interface IFormFilter {
-    setSearcNameQuery: (arg: string) => void;
-    setSearchBrandQuery: (arg: string) => void;
-    setSearchCostQuery: (arg: number) => void;
+    searcQueryMode: string;
+    setSearcQueryMode: (arg: string) => void;
+    setSearchTextRequest: (arg: string) => void;
 }
 
-const FormFilter: React.FC<IFormFilter> = memo(({setSearcNameQuery,
-                                            setSearchBrandQuery,
-                                            setSearchCostQuery,
-                                        }) => {
+const FormFilter: React.FC<IFormFilter> = memo(({searcQueryMode, setSearcQueryMode, setSearchTextRequest}) => {
+
+    const changeSearchNameInput = debounce((e: React.ChangeEvent<HTMLInputElement>) => {
+        const target = e.target as HTMLInputElement
+        setSearchTextRequest(target.value)
+    }, 1500)
+                        
+    const choiseButtonModeSearch = (e: React.MouseEvent<HTMLUListElement>) => {
+        e.preventDefault();
+        const target = e.target as HTMLLinkElement;
+        if (target.tagName === "A") setSearcQueryMode(target.id);
+    }
+
     return(
         <form className={st.formSearh}>
-            <SearchName setSearcNameQuery={setSearcNameQuery}/>
-            <SearchBrand setSearchBrandQuery={setSearchBrandQuery}/>
-            <SearchCost setSearchCostQuery={setSearchCostQuery}/>
+            <div className={st.search}>
+                <label className={st.title} htmlFor="search">Искать по: </label>
+                <div className={st.switcher_mode_search}>
+                    <ul className={st.switcher_mode_search_list} onClick={choiseButtonModeSearch}>
+                        <li><a id={"product"} href="#">Имени</a></li> 
+                        <li><a id={"price"} href="#">Цене</a></li> 
+                        <li><a id={"brand"} href="#">Бренду</a></li> 
+                    </ul>
+                </div>
+                <input className={st.search_input} onChange={changeSearchNameInput} type={searcQueryMode === "price" ? "number" : "text"} id="search"/>
+            </div>
         </form>
     )
 })
-
 
 
 export default FormFilter

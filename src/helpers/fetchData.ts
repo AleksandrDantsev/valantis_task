b593c98e303952c18020dbf695c9fdb1fb47fd91
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-03-10 16:19:21 +0300

    feat: add repeated request to server

--- src/helpers/fetchData.ts
+++ src/helpers/fetchData.ts
@@ -6,9 +6,9 @@ class FetchValantis {
     private url = "http://api.valantis.store:40000/";
     private key = KEY_ACCESS;
 
-    public async get(method: string, params: any) {
+    public async get(method: string, params: {[key: string] : string | number | string[] | undefined[]}): Promise<any> {
+        try {
             const fetchResult = await fetch(this.url, {
-
             method: "POST", 
                 headers: {
                     "X-Auth": this.key,
@@ -18,45 +18,46 @@ class FetchValantis {
             body: JSON.stringify({
                 "action": method,
                 "params": params,
-            }),
-        
-        })
+            }),   
+            })
 
-        const gotArrayProduct = await fetchResult.json()
-        const array = gotArrayProduct?.result;
+            const gotArrayProduct = await fetchResult.json()
+            const array = gotArrayProduct?.result;
 
+            if (method === "get_ids" || method === "filter") {  
+                const hash: {[key: string]: number} = {}
 
-        if (method === "get_ids" || method === "filter") {  
-            const hash: {[key: string]: number} = {}
-
-            for (let item of array) {
-                if (!hash[item]) hash[item] = 1
+                for (let item of array) {
+                    if (!hash[item]) hash[item] = 1
+                }
+                return Object.keys(hash).slice(0, 50)
             }
-            return Object.keys(hash).slice(0, 50)
+
+            else if (method === "get_items") {
+                const resultArray: any = [array[0]]
+                
+                for (let i = 0; i < array.length; i++) {
+                    if (!resultArray.find((el: productsInfoType) => el.id == array[i].id)) resultArray.push(array[i])
+                }
+                return resultArray
+            }     
         }
 
+        catch(error) {
+            console.log(error);
 
-        else if (method === "get_items") {
-            const resultArray: any = [array[0]]
-            
-            for (let i = 0; i < array.length; i++) {
-                if (!resultArray.find((el: productsInfoType) => el.id == array[i].id)) resultArray.push(array[i])
+            let result;
+            while (!result) {
+                result = this.get(method, params);
             }
-            console.log(resultArray)
-            return resultArray
+            return result;
         }
-
+    
+        return [undefined];
     }
 }
 
-
 const Fetch = new FetchValantis()
-
-
-
-
-
-
 
 
 export {Fetch}

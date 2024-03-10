c1794bbcd17fbdba16b409161ef0b0ad302a3260
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-03-10 16:23:20 +0300

    feat: change behavior form buttons

--- src/components/forms/FormFilter/FormFilter.tsx
+++ src/components/forms/FormFilter/FormFilter.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import React, { memo, useRef } from 'react';
 import st from "./FormFilter.module.scss";
 import { debounce } from '../../../helpers/debounce';
 
@@ -9,6 +9,7 @@ interface IFormFilter {
 }
 
 const FormFilter: React.FC<IFormFilter> = memo(({searcQueryMode, setSearcQueryMode, setSearchTextRequest}) => {
+    const input = useRef<HTMLInputElement | null>(null)
 
     const changeSearchNameInput = debounce((e: React.ChangeEvent<HTMLInputElement>) => {
         const target = e.target as HTMLInputElement
@@ -18,21 +19,34 @@ const FormFilter: React.FC<IFormFilter> = memo(({searcQueryMode, setSearcQueryMo
     const choiseButtonModeSearch = (e: React.MouseEvent<HTMLUListElement>) => {
         e.preventDefault();
         const target = e.target as HTMLLinkElement;
-        if (target.tagName === "A") setSearcQueryMode(target.id);
-    }
+        if (target.tagName === "A") {
+            if (input.current !== null) input.current.value = ''
 
+            if (target.id === searcQueryMode) {
+                setSearchTextRequest('');
+                setSearcQueryMode('');
+            }
+            else setSearcQueryMode(target.id);
+        }
+    }
+    console.log(searcQueryMode)
     return(
         <form className={st.formSearh}>
             <div className={st.search}>
                 <label className={st.title} htmlFor="search">Искать по: </label>
                 <div className={st.switcher_mode_search}>
                     <ul className={st.switcher_mode_search_list} onClick={choiseButtonModeSearch}>
-                        <li><a id={"product"} href="#">Имени</a></li> 
-                        <li><a id={"price"} href="#">Цене</a></li> 
-                        <li><a id={"brand"} href="#">Бренду</a></li> 
+                        <li><a className={searcQueryMode === "product" ? st.active_filter : ''} id={"product"} href="#">Имени</a></li> 
+                        <li><a className={searcQueryMode === "price" ? st.active_filter : ''} id={"price"} href="#">Цене</a></li> 
+                        <li><a className={searcQueryMode === "brand" ? st.active_filter : ''} id={"brand"} href="#">Бренду</a></li> 
                     </ul>
                 </div>
-                <input className={st.search_input} onChange={changeSearchNameInput} type={searcQueryMode === "price" ? "number" : "text"} id="search"/>
+                <input className={Boolean(searcQueryMode) === false ? st.search_input + ' ' + st.deactive_input : st.search_input}
+                       onChange={changeSearchNameInput}
+                       placeholder='Запрос'
+                       type={searcQueryMode === "price" ? "number" : "text"} 
+                       id="search"
+                       ref={input} />
             </div>
         </form>
     )

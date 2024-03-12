4596b95bbbeb23385e10b2d182219eb1f6cab320
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-03-12 10:52:11 +0300

    chore: change format images

--- src/imports/importsImage.ts
+++ src/imports/importsImage.ts
@@ -1,13 +1,13 @@
-import colie from "../assets/colie.jpg"
-import ring from "../assets/ring.webp"
-import braslet from "../assets/braslet.jpg"
-import brosh from "../assets/brosh.png"
-import sergi from "../assets/sergi.jpg"
-import culon from "../assets/culon.jpg"
-import chain from "../assets/chain.jpg"
-import complect from "../assets/complect.jpg"
-import stable from "../assets/stable.jpg"
-import bej from "../assets/bej.jpg"
+import colie from "/colie.webp"
+import ring from "/ring.webp"
+import braslet from "/braslet.webp"
+import brosh from "/brosh.png"
+import sergi from "/sergi.webp"
+import culon from "/culon.webp"
+import chain from "/chain.webp"
+import complect from "/complect.webp"
+import stable from "/stable.webp"
+import bej from "/bej.webp"
 
 const importsImage: {[key: string]: string} = {
     "колье": colie,
